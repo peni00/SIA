5038ab8143f6549c88ed38ee6f60dcb4da2b7fe1
jayps69 <131273253+jayps69@users.noreply.github.com>
2023-04-26 16:11:34 +0800

    Update Archives-Services.php
    Complete connected to services

--- Admin/GYM/GYM/Archives-Services.php
+++ Admin/GYM/GYM/Archives-Services.php
@@ -1,3 +1,15 @@
+<?php
+$conn = mysqli_connect("sbit3f-gym.ctwnycxphco9.ap-southeast-1.rds.amazonaws.com","admin","sbit3fruben","sbit3f");
+
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 
@@ -39,72 +51,115 @@
                 </a>
             </ul>
             <div class="admin">
-                <img src="images/image10.png" class="user" style="width: 40px ">
-                <button style="font-weight: 700">Admin Rod <div class="dropdown">
-                        <img src="images/dropd.png" alt="dropdown icon" class="dropdown-icon">
-                        <div class="dropdown-content">
-                            <a class="dropdown-item"
-                                href="http://localhost/SIA/Admin/log_In/Profile1.php">View&nbsp;&nbsp;Profile</a>
-                            <a class="dropdown-item" onclick="return confirm('Are you sure to logout?');"
-                                href="logout.php">Logout</a>
-                        </div>
-                    </div>
+                <img src="images/image10.png" class="user" style="width: 40px">
+                <button style="font-weight: 700">Admin Rod
+                    <img src=" images/dropd.png" style="width:25px">
                 </button>
             </div>
-            <!--sidebar-->
+        </div>
+        <!--sidebar-->
 
-            <div class="container">
-                <a href="http://localhost/SIA/Admin/log_In/homepage.php" type="button" class="back-btn"><img
-                        src="images/back-btn-gray.png" style="width: 30px"> </a>
-                <h3 style="color:#0C0C0C">Home / <a href="#" style="color:#349EFF">Appointment</a></h3>
-                <div class="ARCHIVE">
-                    <a href="#"><button class="serbtn">SERVICE</button></a>
-                    <a href="http://localhost/SIA/Admin/GYM/GYM/Archives-Staff.php"><Button
-                            class="stabtn">STAFF</Button></a>
-                    <a href="http://localhost/SIA/Admin/GYM/GYM/Archives-Appointment.php"><Button
-                            class="appbtn">APPOINTMENT</Button></a>
-                </div>
+        <div class="container">
+            <a href="http://localhost/SIA/Admin/log_In/homepage.php" type="button" class="back-btn"><img
+                    src="images/back-btn-gray.png" style="width: 30px"> </a>
+            <h3>Home / <a href="#" style="color:#349EFF">Appointment</a></h3>
+            <div class="ARCHIVE">
+                <a href="#"><button class="serbtn">SERVICE</button></a>
+                <a href="http://localhost/SIA/Admin/GYM/GYM/Archives-Staff.php"><Button
+                        class="stabtn">STAFF</Button></a>
+                <a href="http://localhost/SIA/Admin/GYM/GYM/Archives-Appointment.php"><Button
+                        class="appbtn">APPOINTMENT</Button></a>
             </div>
-            <!--container-->
+        </div>
+       <!--container-->
 
-            <div class="table-container">
+<div class="table-container">
+
+<form method="POST">
+   <button type="submit" class="unbtn" name="delete" value="Delete" onclick="return confirm('ARE YOU SURE YOU WANT TO UNARCHIVED THIS ITEM/S!')">UNARCHIVED</button>
+
+    <table>
+        <thead>
+            <tr>
+                <th width="200"></th>
+                <th>SERVICE ID</th>
+                <th>SERVICES</th>
+                <th>DETAILS</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Query to retrieve data from servicetypetbl
+            $sql = "SELECT * FROM servicetypetblarchive";
+
+            // Execute the query and get the result set
+            $result = mysqli_query($conn, $sql);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<tr>';
+                echo "<td><input type='checkbox' name='check[]' value='" . $row['serviceID'] . "'>&nbsp;&nbsp;&nbsp;";
+                echo '<td>' . $row['serviceID'] . '</td>';
+                echo '<td>' . $row['serviceType'] . '</td>';
+                echo '<td>' . $row['serviceDescription'] . '</td>';
+                echo '</tr>';
+            }
+            ?>
+
+        </tbody>
+    </table>
+	</form>
+	
+	 <!-- DELETE -->
+
+    <?php
+     if(isset($_POST['delete']))
+{
+    $all_id = $_POST['check'];
+    $extract_id = implode(',' , $all_id);
+
+    // Perform the insert operation
+    $insert_query = "INSERT INTO servicetypetbl (serviceID, serviceType, serviceDescription)
+                     SELECT serviceID, serviceType, serviceDescription
+                     FROM servicetypetblarchive
+                     WHERE serviceID IN($extract_id)";
+					 
+    $insert_query_run = mysqli_query($conn, $insert_query);
+	
+	// Perform the delete operation
+    $query = "DELETE FROM servicetypetblarchive WHERE serviceID IN($extract_id) ";
+    $query_run = mysqli_query($conn, $query);
+
+    // Check if both operations were successful
+    if($query_run && $insert_query_run)
+    {
+        echo "Multiple Data Deleted and Archived Successfully";
+        header('Location: http://localhost/SIA/Admin/GYM/GYM/Archives-Services.php');
+        exit();
+    }
+    else if(!$insert_query_run)
+    {
+        echo "Multiple Data Deleted, but Archive Failed: " . mysqli_error($conn);
+    }
+    else
+    {
+        echo "Multiple Data Not Deleted or Archived";
+    }
+}
 
 
-                <button type="button" class="unbtn">UNARCHIVE</button>
+       ?>  
+</div>
 
-                <table>
-                    <thead>
-                        <tr>
-                            <th width="200"></th>
-                            <th>SERVICE ID</th>
-                            <th>SERVICES</th>
-                            <th>DETAILS</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><input type="checkbox"></td>
-                            <td>SAMPLE</td>
-                            <td>SAMPLE</td>
-                            <td>SAMPLE</td>
-                        </tr>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
 
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-                integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-                crossorigin="anonymous">
-            </script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-                integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
-                crossorigin="anonymous">
-            </script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-                integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
-                crossorigin="anonymous">
-            </script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+            integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+            integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+            integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
