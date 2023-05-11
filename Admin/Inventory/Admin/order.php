fc5231cf14ce878dbba304b931a346a6fa3dd317
Angelo <angelopino044@gmail.com>
2023-05-11 12:53:30 +0800

    update the order
    latest order side

--- Admin/Inventory/Admin/order.php
+++ Admin/Inventory/Admin/order.php
@@ -36,11 +36,7 @@ include('includes/menubar.php');
 
 
 
-								 $query = "SELECT transaction.* , account .Fname, account .Lname
-								 FROM transaction transaction
-								 INNER JOIN account account  ON account .Account_ID = transaction.Account_ID
-								 GROUP BY Order_ID
-								 ORDER BY Date DESC";
+								 $query = "SELECT * FROM `transaction` Group By Order_ID  order by Date desc;";
 
 								 $result = $conn->query($query);
 
@@ -49,10 +45,7 @@ include('includes/menubar.php');
 								 while($row = $result->fetch_assoc())
 								 {
 
-									$fullname = $row["Fname"] . ", " . $row["Lname"];
-
 									 $orderstatus = $row['Status'];
-
 									 $address = $row["Street"] . ", " . $row["Barangay"] . ", " . $row["City"]. ", " .$row["Zip_Code"]."<br>";
 
 									 if($orderstatus ==  "Delivered"){
@@ -71,12 +64,17 @@ include('includes/menubar.php');
 
                                         <td class="text-center"><?php echo $i++ ?></</td>
                                         <td class="text-center"><a  href="order_details.php?id=<?php echo $row['Order_ID'] ?>"><?php echo $row['Order_ID'] ?></a></td>
-                                        <td class="text-center"><?php echo $fullname ?></td>
+                                        <td class="text-center"><?php echo $row['Name'] ?></td>
                                         <td class="text-center"><?php echo $row['Date'] ?></td>
 										<td class="text-center"><?php echo $address ?></td>
 										<td class="text-center"><?php echo $row['Payment_Method'] ?></td>
 										<td class="text-center"> <span class="badge <?php echo $badge_class ?>"><?php echo $row['Status'] ?></span></td>
+
+										<?php if($orderstatus ==  "Delivered"){?>
+										<td class="text-center"><button class="btn btn-secondary btn-sm btn-flat" disabled>Received</button></td>
+										<?php }else{?>
 										<td class="text-center"><a class="btn btn-primary btn-sm btn-flat" href="updateorder.php?id=<?php echo $row['Order_ID'];?>">Update</a></td>
+										<?php }?>
                                     </tr>
                                     <?php
                                 	 }
--- Admin/Inventory/Admin/updateorder.php
+++ Admin/Inventory/Admin/updateorder.php
@@ -5,14 +5,19 @@
 
     session_start();
 
-	$Order_ID=$_GET['id'];
+	$Order_ID = $_GET['id'];
 
+	$order_query = mysqli_query($con, "SELECT account.*, transaction.*, products.*
+									   FROM transaction
+									   INNER JOIN account ON account.Account_ID = transaction.Account_ID
+									   INNER JOIN products ON products.id = transaction.Product_ID
+									   WHERE transaction.Order_ID = '$Order_ID'");
 
-	$order_query = mysqli_query($con, "SELECT  account.*, transaction.* FROM transaction INNER JOIN account ON account.Account_ID = transaction.Account_ID WHERE Order_ID ='$Order_ID'");
 	if(mysqli_num_rows($order_query) >= 0)
 	{
 	while($order_item = mysqli_fetch_assoc($order_query))
 	{
+	$product_name[] = $order_item['name'];
 	$Email_Add = $order_item['Email_Add'];
 	$Account_ID = $order_item['Account_ID'];
 	$Order_ID = $order_item['Order_ID'];
@@ -24,6 +29,8 @@
 	$Price = $order_item['Price'];
 	$Status = $order_item['Status'];
 
+    $product_price = $order_item['Price'] * $order_item['Qty'];
+	$price_total += $product_price;
 
 	if ($Status == "To Pack"){
     $update_query="Update transaction SET Status = 'Ready to Ship' WHERE Order_ID ='$Order_ID'";
@@ -34,8 +41,23 @@
 	global $con;
 	$email = $Email_Add;
 	$email = addslashes($email);
+    $total_product = implode(',',$product_name);
+
 	//send email here
-	send_mail($email,'Order Status',"Your Order is Ready to Ship");
+
+	send_mail($email,'Order Status' ,"<h3>Your Order Identification # " .$Order_ID. " is Ready to Ship</h3><div class='order-message-container'>
+      <div class='message-container'>
+         <h3>Thank you for shopping!</h3>
+         <div class='order-detail'>
+            <span>".$total_product."</span>
+         </div>
+         <div class='customer-details'>
+		    <span class='total'> total :  ₱".$price_total." </span>
+            <p>(*pay when product arrives*)</p>
+         </div>
+         </div>
+      </div>, Pls wait for your order to arrived, Thank you.");
+
 
 
     header('location: ../Admin/order.php');
@@ -49,8 +71,19 @@
 	$email = $Email_Add;
 	$email = addslashes($email);
 	//send email here
-	send_mail($email,'Order Status',"Your Order is Out for Delivery");
 
+	send_mail($email,'Order Status' ,"<h3>Your Order Identification # " .$Order_ID. "  is Out for Delivery</h3><div class='order-message-container'>
+      <div class='message-container'>
+         <h3>Thank you for shopping!</h3>
+         <div class='order-detail'>
+            <span>".$total_product."</span>
+         </div>
+         <div class='customer-details'>
+		    <span class='total'> total :  ₱".$price_total." </span>
+            <p>(*pay when product arrives*)</p>
+         </div>
+         </div>
+      </div>, Pls wait for your order to arrived, Thank you.");
 
     header('location: ../Admin/order.php');
 	}else if ($Status == "Out for Delivery"){
@@ -62,9 +95,21 @@
 	global $con;
 	$email = $Email_Add;
 	$email = addslashes($email);
-	//send email here
-	send_mail($email,'Order Status',"Your Order is Delivered");
 
+	//send email here
+
+	send_mail($email,'Order Status' ,"<h3>Your Order Identification # " .$Order_ID. " is Delivered</h3><div class='order-message-container'>
+      <div class='message-container'>
+         <h3>Thank you for shopping!</h3>
+         <div class='order-detail'>
+            <span>".$total_product."</span>
+         </div>
+         <div class='customer-details'>
+		    <span class='total'> total :  ₱".$price_total." </span>
+            <p>(*pay when product arrives*)</p>
+         </div>
+         </div>
+      </div>, Pls wait for your order to arrived, Thank you.");
 
     header('location: ../Admin/order.php');
 	}else{
@@ -72,15 +117,6 @@
 	$update_query="INSERT INTO `sales`(`Account_ID`, `Order_ID`, `Product_ID`, `Date`, `Payment_Method`, `Qty`, `Total`, `Price`)
 	VALUES ('$Account_ID','$Order_ID','$Product_ID','$Date','$Payment_Method','$Qty','$Total','$Price')";
     $update_query_result=mysqli_query($con,$update_query) or die(mysqli_error($con));
-
-
-
-	global $con;
-	$email = $Email_Add;
-	$email = addslashes($email);
-	//send email here
-	send_mail($email,'Order Status',"Your Order is Delivered");
-
 
 
 	// $update_query="delete from transaction WHERE Order_ID ='$Order_ID'";
