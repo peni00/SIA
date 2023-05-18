4db79f500de8fc2ad6111e3dda488213a33aa1a8
Jayps69 <131273253+jayps69@users.noreply.github.com>
2023-05-18 19:49:02 +0800

    Update Archives-Services.php
    Archives-Feedback button added

--- Admin/GYM/GYM/Archives-Services.php
+++ Admin/GYM/GYM/Archives-Services.php
@@ -77,6 +77,8 @@ if (mysqli_connect_errno()) {
                         class="stabtn">STAFF</Button></a>
                 <a href="http://18.136.105.108:81/SIA/Admin/GYM/GYM/Archives-Appointment.php"><Button
                         class="appbtn">APPOINTMENT</Button></a>
+						<a href="http://18.136.105.108:81/SIA/Admin/GYM/GYM/Archives-Feedback.php"><Button
+                        class="feedbtn">FEEDBACK</Button></a>
             </div>
         </div>
        <!--container-->
